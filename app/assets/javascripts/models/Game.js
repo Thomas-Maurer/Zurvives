fc399450f05fb685e9e64f50688f6d3528f11c82
SamiErrighi <sami.errighi@gmail.com>
2015-06-17 11:48:51 +0200

    init route

---
+++ app/assets/javascripts/models/Game.js
@@ -0,0 +1,19 @@
+var Game = Class.extend({
+
+    init: function(players){
+        players = typeof players !== 'undefined' ? players : [];
+        this.players = players
+        this.nbPlayer = this.players.length;
+    },
+
+    start: function() {
+    },
+
+    randomTurn: function() {
+
+        _.sample(this.players);
+    }
+});
+
+var test = new Game();
+test.randomTurn();
---
+++ app/assets/javascripts/models/Player.js
@@ -0,0 +1,10 @@
+var Player = Class.extend({
+    init: function(isDancing){
+        this.dancing = isDancing;
+    },
+    dance: function(){
+        return this.dancing;
+    }
+});
+
+
